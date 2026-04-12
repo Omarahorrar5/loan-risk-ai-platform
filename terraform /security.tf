04b2ede711b7d2dbb507bf6db300d25bef136978
Omarahorrar5 <omarahorar@gmail.com>
2026-04-12 17:42:31 +0100

    adding terraform security [skip actions]

---
+++ terraform /security.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "main" {
+  name        = "${var.project_name}-sg"
+  description = "Security group for loan risk platform"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  ingress {
+    description = "FastAPI Backend"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+    Project = var.project_name
+  }
+}
